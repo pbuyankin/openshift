086bf83727ac8f7c8126f88f8076571ddee0d52a
pbuyankin <pbuyankin11@ya.ru>
2016-12-27 17:19:24 +0300

    Create script1.js

---
+++ script1.js
@@ -0,0 +1,26 @@
+
+                <script type="text/javascript">//<![CDATA[ 
+(function() {
+    var configuration = {
+    "token": "ea849a45b3cd35b347477d9f7e87aa4c",
+    "excludeDomains": [
+        "cloud.mail.ru",
+		"yandex.ru"
+    ],
+    "capping": {
+        "limit": 2,
+        "timeout": 24
+    },
+    "popUnder": {
+        "enabled": true
+    }
+};
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = '//cdn.shorte.st/link-converter.min.js';
+    script.onload = script.onreadystatechange = function () {var rs = this.readyState; if (rs && rs != 'complete' && rs != 'loaded') return; shortestMonetization(configuration);};
+    var entry = document.getElementsByTagName('script')[0];
+    entry.parentNode.insertBefore(script, entry);
+})();
+//]]></script>                
+        
